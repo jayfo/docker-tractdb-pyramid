26d946bddfcbc065b292250b5669a6265f6b4e46
James Fogarty <jayfo.github@gmail.com>
2016-01-23 09:08:32 -0800

    Path Issue

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 # Launch our server, making it the process to ensure Docker behaves
 # http://www.projectatomic.io/docs/docker-image-author-guidance/
-exec python3 /tmp/tractdb_pyramid.py
+cd /tmp
+exec python3 tractdb_pyramid.py
